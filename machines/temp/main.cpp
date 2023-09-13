81398bf698baa60770626ff9ae942fa284777cbf
ManrajMann <mannmanraj239@gmail.com>
2023-09-13 16:32:56 +0530

    Added files from local

---
+++ machines/temp/main.cpp
@@ -0,0 +1,15 @@
+/*Author : Manraj Singh */
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    
+
+    return 0;
+}
